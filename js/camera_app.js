00bffc2d4e5dd25b2a0e96971778d93753b9850f
Tai Ratcliff <32991623+tairatcliff-labrat@users.noreply.github.com>
2018-11-11 20:13:12 +1100

    Add files via upload

--- js/camera_app.js
+++ js/camera_app.js
@@ -22,7 +22,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = cameraSensor.toDataURL("images/webp");
     cameraOutput.classList.add("taken");
 };
 // Start the video stream when the window loads
